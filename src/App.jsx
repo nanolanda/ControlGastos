dd0caefab57c519e9873ee16a7ad8dfd42c2db96
Alvaro Esteban Garzon Ramirez <nanolanda05@gmail.com>
2022-02-06 23:31:34 +0100

    Added gastos and editing them

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,105 @@
-import { useState } from 'react'
+import { object } from 'prop-types';
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
+import ListadoGastos from './components/ListadoGastos';
+import Modal from './components/Modal';
+import { generarId } from './helpers';
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+
+  const [gastos, setGastos] = useState([]);
+  const [gastoEditar, setGastoEditar] = useState({});
+
+  useEffect(() => {
+    if(Object.keys(gastoEditar).length > 0){
+      setModal(true);
+
+      setTimeout(() => {
+          setAnimarModal(true);
+      }, 500);
+    }
+  }, [gastoEditar]);
+
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+    setGastoEditar({});
+
+    setTimeout(() => {
+        setAnimarModal(true);
+    }, 500);
+  }
+
+  const guardarGasto = gasto => {
+    if(gasto.id){
+      const gastosActualizados = gastos.map(gastoState => gastoState.id === gasto.id ? gasto:gastoState);
+      setGastos(gastosActualizados);
+      setGastoEditar({});
+    }else{
+      gasto.id = generarId();
+      gasto.fecha = Date.now();
+      setGastos([...gastos, gasto]) 
+    }
+
+    setAnimarModal(false);
+    setTimeout(() => {
+      setModal(false);            
+    }, 500);
+    
+  }
+
+  const eliminarGasto = id => {
+    const gastosActualizados = gastos.filter(gasto => gasto.id !== id);
+    setGastos(gastosActualizados);
+  }
+
   return (
-    <div>
+    <div className={modal ? 'fijar':''}>
       <Header
         presupuesto = {presupuesto}
         setPresupuesto = {setPresupuesto}
         isValidPresupuesto = {isValidPresupuesto}
         setIsValidPresupuesto = {setIsValidPresupuesto}
+        gastos={gastos}
       />
+      {isValidPresupuesto && (
+        <>
+          <main>
+            <ListadoGastos
+              gastos={gastos}
+              setGastoEditar = {setGastoEditar}
+              eliminarGasto = {eliminarGasto}
+            />
+          </main>
+
+          <div className= 'nuevo-gasto'>
+            <img  
+                  src={IconoNuevoGasto} 
+                  alt="icono nuevo gasto" 
+                  onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
+      )}
+
+      {modal && (
+        <Modal
+          setModal={setModal}
+          animarModal = {animarModal}
+          setAnimarModal = {setAnimarModal}
+          guardarGasto ={guardarGasto}
+          gastoEditar = {gastoEditar}
+          setGastoEditar = {setGastoEditar}
+        />)
+      }
+      
     </div>
   )
 }
--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,16 +1,28 @@
-const ControlPresupuesto = ({presupuesto}) => {
+import { useState, useEffect } from "react";
+import { formatearCantidad } from "../helpers";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css'
 
-  const formatearCantidad = (cantidad) => {
-    return cantidad.toLocaleString('es-EU', {
-        style: 'currency',
-        currency: 'EUR'
-    }) 
-  };
+const ControlPresupuesto = ({presupuesto, gastos}) => {
+
+    const [porcentaje, setPorcentaje] = useState(0);
+    const [disponible, setDisponible] = useState(0);
+    const [gastado, setGastado] = useState(0);
+
+    useEffect(() => {
+        const totalGastado = gastos.reduce((total,gasto) => gasto.cantidad + total, 0);
+        const totalDisponible = presupuesto - totalGastado;
+        setDisponible(totalDisponible);
+        setGastado(totalGastado);
+    }, [gastos]);
+    
 
   return (
   <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
-            <p>Grafica aqui</p>
+            <CircularProgressbar
+                value = {porcentaje}
+            />
       </div>
 
       <div className="contenido-presupuesto">
@@ -18,10 +30,10 @@ const ControlPresupuesto = ({presupuesto}) => {
                 <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
             </p>
             <p>
-                <span>Disponible: </span> {formatearCantidad(0)}
+                <span>Disponible: </span> {formatearCantidad(disponible)}
             </p>
             <p>
-                <span>Gastado: </span> {formatearCantidad(0)}
+                <span>Gastado: </span> {formatearCantidad(gastado)}
             </p>
       </div>
   </div>
